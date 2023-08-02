372b0b80094ce997e4e618d31b7263e724a21464
J-Schlechter <j.schlechter.97@gmail.com>
2023-08-02 06:25:19 +0200

    dawd
    awd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello Bulma!</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+<<<<<<< Updated upstream
   </head>
   <body style="background-image: url('https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg');">
 
@@ -13,6 +14,50 @@
     <div id="app">
       <div>
       <app></app>
+=======
+    <title>Home</title>
+</head>
+<body>
+    
+    @auth
+    <nav class="navbar " role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+          
+            </a>      
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="true" data-target="navbarBasicExample">
+            
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        
+      
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item" href="/">Home</a>
+            <a class="navbar-item" href="#">Write a new post</a>
+            </div>
+          </div>
+      
+          <div class="navbar-end">
+            <div class="navbar-item">                    
+                    <div class="buttons" >
+                        <form action = "/logout" method = "POST">
+                          @csrf
+                            <button class="button is-light" onclick="/logout">
+                            Log Out
+                            </button>
+                        </form>
+                        
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+>>>>>>> Stashed changes
       </div>
       <h1>Hellos</h1>
     </div>
